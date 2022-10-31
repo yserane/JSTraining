776f6b01d699b1ab95c62c2b3c4f8e5899a5cf85
yserane <55660730+yserane@users.noreply.github.com>
2022-10-31 20:23:37 +0200

    practice-3-dom

--- Day1/Exercise1/exercise.js
+++ Day1/Exercise1/exercise.js
@@ -28,8 +28,36 @@ let course = {
             graduationYear: 2023
          },
          {
-            name:"me",
+            name:"you",
             age: 20,
             graduationYear: 2021
-         }]
+         }], 
+         toString: function(){
+            return "Course name "+this.name +" taught by: " +this.teacher.name;
+         }
 };
+
+console.log(course.name, course.createdYear);
+console.log(course.teacher.name);
+
+let students = course.student;
+for(let index =0; index< students.length; index++) {
+   console.log(students[index].name +"\n"); 
+}
+let sum=0;
+let studentLength = students.length;
+
+for(let index =0; index< studentLength; index++) {
+    sum += students[index].age;
+}
+
+console.log(sum/studentLength);
+ 
+let max = students[0].graduationYear;
+for(let index =1; index< studentLength; index++) {
+    if (students[index].graduationYear> max)
+        max = students[index].graduationYear;
+}
+
+
+console.log(course.toString());
---
+++ Day2/JSHTMLPractice/practice.js
@@ -0,0 +1,10 @@
+//get element by id
+// get element by tag name
+// get element by classname
+// get value in a span
+// change color of the text in the span
+// hide.show an element on click
+// add to dom
+// add eventlistener to element
+//remove from dom 
+
